7ab1767d5e9cf22fe4273bf2458abf658de43e24
tfang7 <fangt@rpi.edu>
2015-12-09 12:25:46 -0500

    added rotating cogs to the level

---
+++ GameDev Project 4/Assets/CogRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CogRotation : MonoBehaviour {
+    int SPEED = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(((this.transform.rotation.z + SPEED) * Time.deltaTime), 0, 0));
+
+
+    }
+}
